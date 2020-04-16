8f08c15154e3b8207febf64b864ab581e2484aa6
nonaka-yasumichi <nonaka_yasumichi@yahoo.co.jp>
2020-04-16 22:55:40 +0900

    update

--- corona/corona.js
+++ corona/corona.js
@@ -1,3 +1,13 @@
 /* 週次見える化 */
+var SUMMARY_CSV_URL= "https://github.com/kaz-ogiwara/covid19/raw/master/data/summary.csv";
+
+function getCsvStr(url){
+	var xhr = new XMLHttpRequest();
+	xhr.open("get",url,false);
+	xhr.send(null);
+	return xhr.response;
+}
 function onLoadFunction(){
+	var csvStr = getCsvStr(SUMMARY_CSV_URL);
+	console.log(csvStr);
 }
